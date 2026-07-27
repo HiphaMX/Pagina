dfe1c75265ad67b5b3cee02cb897c0512b1d3efc
Fanssi Marketing <efe.creativo@gmail.com>
2026-07-27 11:35:27 -0600

    trigger: force Vercel build by changing main.py

--- app/main.py
+++ app/main.py
@@ -97,5 +97,7 @@ def read_root():
 if os.path.exists("projects"):
     app.mount("/projects", StaticFiles(directory="projects"), name="projects")
 
+# trigger: force vercel rebuild outside projects folder
+
 
 
